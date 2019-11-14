cd0322e35262bd1c3f694aa6232f4b46381ecbd7
Ghania Riaz <ghaniariaz1@gmail.com>
2019-11-14 16:04:10 +0100

    add button to boostview

--- src/scenes/BoostView.js
+++ src/scenes/BoostView.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ViewTitle from "../components/ViewTitle";
 import ViewMessage from "../components/ViewMessage";
+import Button from "../components/Button";
+
+
 
 function BoostView() {
     return (
         <div>
             <ViewTitle/>
             <ViewMessage/>
+            <Button/>
         </div>
     );
 }
