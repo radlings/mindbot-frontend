6f59d393a865d1e82888f194160c4239473b36d7
Ghania Riaz <ghaniariaz1@gmail.com>
2019-11-15 17:38:52 +0100

    Interact with service to get the category listings

--- src/scenes/BoostView.js
+++ src/scenes/BoostView.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ViewTitle from "../components/ViewTitle";
 import ViewMessage from "../components/ViewMessage";
 import Button from "../components/Button";
+import {getListings} from "../services/categoriesListService";
 
 
 
 function BoostView() {
+    let categories = getListings();
     return (
         <div>
             <ViewTitle/>
